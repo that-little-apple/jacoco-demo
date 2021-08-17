94b427593f3bfc16222f935120eac66a2d4a4818
xiaoyao62 <xiaoyao62@jd.com>
2021-08-17 11:07:50 +0800

    初始化提交

---
+++ README.md
@@ -0,0 +1,2 @@
+本项目为jacoco demo项目，参考阔海老师代码库:https://gitee.com/shuhaihuang/cyber-range-jacoco?_from=gitee_search
+
---
+++ src/test/java/com/example/jacoco/JacocoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jacoco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacocoDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
